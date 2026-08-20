e3f08c35384c209f5d86dd0c46d58cf2870723a4
famanoa95-spec <famanoa95@gmail.com>
2026-08-20 13:56:50 +0200

    add account and transaction models

--- src/main/java/com/example/examen_poo_avecia/model/transaction.java
+++ src/main/java/com/example/examen_poo_avecia/model/transaction.java
@@ -1,5 +1,44 @@
 package com.example.examen_poo_avecia.model;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class transaction {
 
+    private String id;
+
+    private Instant createdAt;
+
+    @NotNull
+    private transactionType transactionType;
+
+    @NotNull
+    @DecimalMin(value = "0.0", inclusive = false, message = "Le montant doit être positif")
+    private BigDecimal amount;
+
+    @NotBlank
+    private String reason;
+
+    @NotNull
+    private String accountId;
+
+    public transaction(transactionType transactionType, BigDecimal amount, String reason, String accountId) {
+        this.id = UUID.randomUUID().toString();
+        this.createdAt = Instant.now();
+        this.transactionType = transactionType;
+        this.amount = amount;
+        this.reason = reason;
+        this.accountId = accountId;
+    }
 }
