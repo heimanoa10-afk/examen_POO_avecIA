36a1deeaf64792367186bb52f68d91260238bd6a
nerayn4 <hei.nyharena@gmail.com>
2026-08-20 15:47:29 +0300

    my first commit

---
+++ src/main/java/org/example/examen_poo_avecia/ExamenPooAvecIaApplication.java
@@ -0,0 +1,13 @@
+package org.example.examen_poo_avecia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenPooAvecIaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenPooAvecIaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/examen_poo_avecia/ExamenPooAvecIaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.examen_poo_avecia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenPooAvecIaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
