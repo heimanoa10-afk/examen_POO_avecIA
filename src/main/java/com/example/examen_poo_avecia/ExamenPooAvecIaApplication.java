d200e087771ea9955cd78eaa93ff7e9a9da99153
famanoa95-spec <famanoa95@gmail.com>
2026-08-20 12:35:47 +0200

    first commit

---
+++ src/main/java/com/example/examen_poo_avecia/ExamenPooAvecIaApplication.java
@@ -0,0 +1,13 @@
+package com.example.examen_poo_avecia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenPooAvecIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenPooAvecIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/examen_poo_avecia/ExamenPooAvecIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examen_poo_avecia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenPooAvecIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
